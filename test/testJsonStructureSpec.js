8a41d82761b2f171211385eac544064f89039dd7
sumitsitesh <sumitstark1994@gmail.com>
2017-01-25 16:48:46 +0530

    commit for CI

--- test/testJsonStructureSpec.js
+++ test/testJsonStructureSpec.js
@@ -2,8 +2,8 @@ var chai = require('chai');
 var expect = chai.expect;
 var jsonDiff = require('./jsondiff');
 var totalObjectKeys = require('./totalObjectKeys');
-var expectedJSON = require('./expectedjson/chichagoCrime1.json');
-var actualJSON = require('../outputdata/ChicagoCrimeStackByYear.json');
+var expectedJSON = require('./expectedjson/expectedJsonSumeet1.json');
+var actualJSON = require('../outputdata/outputJsonSumeet1.json');
 describe('Test Application as Blackbox', function(){
   it ('Test JSON is well formed', function(done){
     /*ToDO Parse JSON*/
